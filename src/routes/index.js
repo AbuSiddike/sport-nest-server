55edb0e2d0ed13ec2dfc39a736f2ac6c9e7ab7b5
AbuSiddike <abubakkarsiddike1997@gmail.com>
2026-05-19 00:20:26 +0600

    add general booking route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const bookingRoutes = require("../modules/bookings/booking.routes");
 
 const router = express.Router();
 
 router.get("/health", (req, res) => {
   res.json({ success: true, message: "SportNest API is running" });
 });
+
+router.use("/bookings", bookingRoutes);
 
 module.exports = router;
